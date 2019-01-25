34420c4ffcd765f6feb00505fbab14223631cee5
sd03sd03 <45281005+sd03sd03@users.noreply.github.com>
2019-01-25 21:01:19 +0900

    Update Ticketing.java

--- src/Ticketing/Ticketing.java
+++ src/Ticketing/Ticketing.java
@@ -1,11 +1,17 @@
 package Ticketing;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import bean.LocationBean;
+import dao.LocationDAO;
 
 /**
  * Servlet implementation class Ticketing
@@ -27,7 +33,17 @@ public class Ticketing extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		int locationNum = Integer.parseInt(request.getParameter("locationNum"));
+		
+		LocationDAO dao = LocationDAO.getInstance();
+		ArrayList<LocationBean> arr = dao.getLocationName(locationNum);
+		
+		request.setAttribute("locationArr", arr);
+		System.out.println();
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("movieTime.jsp");
+		dispatcher.forward(request, response);
+		
 	}
 
 	/**
