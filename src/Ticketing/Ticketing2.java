99b9dea485136bde2342f7fbed59d5e90c9a2231
sd03sd03 <45281005+sd03sd03@users.noreply.github.com>
2019-01-25 19:08:38 +0900

    Create Ticketing2.java

---
+++ src/Ticketing/Ticketing2.java
@@ -0,0 +1,66 @@
+package Ticketing;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bean.LocationBean;
+import dao.LocationDAO;
+
+/**
+ * Servlet implementation class Ticketing2
+ */
+@WebServlet("/movie/Ticketing2.do")
+public class Ticketing2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Ticketing2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+request.setCharacterEncoding("UTF-8");
+		
+		int locationNum = Integer.parseInt(request.getParameter("locationNum"));
+		
+		
+		LocationDAO dao = LocationDAO.getInstance();
+		
+		ArrayList<LocationBean> arr = dao.getLocationName(locationNum);
+		
+		request.setAttribute("arr", arr);
+		String i =  arr.get(0).getLocation_name();
+		System.out.println(i);
+		request.setAttribute("i", i);
+		
+		
+		response.setContentType("text/html;charset=utf-8");
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher("../ticket/Ticketing1.jsp");
+		dispatcher.forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
